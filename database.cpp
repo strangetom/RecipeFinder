4ea11bb40228086d88ffbc52315dcfaf82b2ff3d
Tom Strange <tpstrange@gmail.com>
2018-03-05 20:57:30 +0000

    Change location of images, back to using full size images instead of thumbnails

--- database.cpp
+++ database.cpp
@@ -84,7 +84,7 @@ int db_ops::update_database(QSqlDatabase *db)
         // Extract data from json
         QString json_path = path;
         QString title = j["name"].toString();
-        QString img_path = "thumbnails/" + j["image"].toString();
+        QString img_path = "website/images/" + j["image"].toString();
         QString category = j["category"].toString();
         QString html_path = "website/recipes/" + j["image"].toString().replace("_", " ").replace("jpg", "html");
 
